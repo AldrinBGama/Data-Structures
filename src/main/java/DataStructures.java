35b9efb191becc2e76fe987ca87dac48e174d1d5
aldrinbgama <aldrin.gama@umuzi.org>
2020-02-24 01:25:12 +0200

    second commit

--- src/main/java/DataStructures.java
+++ src/main/java/DataStructures.java
@@ -1,8 +1,8 @@
 public class DataStructures {
     int width, height;
     int [][] board = { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 1, 1, 0, 0, 0, 0, 0 },
             { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 },
             { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
             { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
             { 0, 0, 0, 1, 1, 0, 0, 0, 0, 0 },
@@ -17,22 +17,23 @@ public class DataStructures {
         this.height = height;
     }
     // Function to print next generation
-    static void nextGeneration(int board[][], int height, int width)
-    {
+    static void nextGeneration(int board[][], int height, int width) {
         int[][] future = new int[height][width];
         // Loop through every cell
         int l = 1;
-        while (++l < height - 1) {
+        while (l < height - 1) {
             int m = 1;
-            while (++m < width - 1) {
+            while (m < width - 1) {
                 // finding no Of Neighbours that are alive
                 int aliveNeighbours = 0;
                 int i = -1;
-                while (++i <= 1) {
+                while (i <= 1) {
                     int j = -1;
-                    while (++j <= 1){
+                    while (j <= 1){
                         aliveNeighbours += board[l + i][m + j];
+                        j++;
                     }
+                    i++;
                 }
                 // The cell needs to be subtracted from its neighbours as it was counted before.
                 aliveNeighbours -= board[l][m];
@@ -54,14 +55,15 @@ public class DataStructures {
                     // Remains the same
                 else
                     future[l][m] = board[l][m];
+                m++;
             }
+            l++;
         }
 
         System.out.println("Next Generation");
-        for (int i = 0; i < height; i++)
-        {
-            for (int j = 0; j < width; j++)
-            {
+        for (int i = 0; i < height; i++) {
+            System.out.print("/");
+            for (int j = 0; j < width; j++) {
                 if (future[i][j] == 0)
                     System.out.print("+");
                 else
@@ -74,6 +76,7 @@ public class DataStructures {
     public void printBoard() {
         System.out.println("Initial Generation");
         for (int i = 0; i < height; i++) {
+            System.out.print("/");
             for (int j = 0; j < width; j++) {
                 if (board[i][j] == 0)
                     System.out.print("+");
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        DataStructures dataStructure = new DataStructures(10, 10);
-        dataStructure.printBoard();
+        DataStructures dataStructures = new DataStructures(10, 10);
+        dataStructures.printBoard();
     }
 }
